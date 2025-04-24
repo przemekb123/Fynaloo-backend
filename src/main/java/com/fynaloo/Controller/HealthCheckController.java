b10ad6969cadfd2895c016e73ceca38c52078cdd
przemekb123 <przemyslawbaj8@gmail.com>
2025-04-24 22:42:09 +0200

    fixed HealthCheckController.

--- src/main/java/com/fynaloo/Controller/HealthCheckController.java
+++ src/main/java/com/fynaloo/Controller/HealthCheckController.java
@@ -3,12 +3,14 @@ package com.fynaloo.Controller;
 import com.fynaloo.Dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CheckController {
+@RequestMapping("/api")
+public class HealthCheckController {
     @GetMapping("/health")
-    public ResponseEntity<ApiResponse> healthCheck() {
-        return ResponseEntity.ok(new ApiResponse("OK"));
+    public ResponseEntity<ApiResponse> ping() {
+        return ResponseEntity.ok(new ApiResponse("Pong"));
     }
 }
