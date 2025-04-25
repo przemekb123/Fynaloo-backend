afb268725b15cf524ba1d81bb5281264bb8851fe
przemekb123 <przemyslawbaj8@gmail.com>
2025-04-25 10:11:22 +0200

    fix: public access to api/health endpoint

--- src/main/java/com/fynaloo/Security/SecurityConfig.java
+++ src/main/java/com/fynaloo/Security/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/users/login",
-                                "/api/users/register"
+                                "/api/users/register",
+                                "/api/health"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
